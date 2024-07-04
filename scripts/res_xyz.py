854a59044f13e4c005c69905079b8fe7532f65e3
Haoming <hmstudy02@gmail.com>
2024-07-04 10:28:49 +0800

    improved infotext

--- scripts/res_xyz.py
+++ scripts/res_xyz.py
@@ -3,7 +3,10 @@ import modules.scripts as scripts
 
 def grid_reference():
     for data in scripts.scripts_data:
-        if data.script_class.__module__ in ('scripts.xyz_grid', 'xyz_grid.py') and hasattr(data, "module"):
+        if data.script_class.__module__ in (
+            "scripts.xyz_grid",
+            "xyz_grid.py",
+        ) and hasattr(data, "module"):
             return data.module
 
     raise SystemError("Could not find X/Y/Z Plot...")
@@ -13,14 +16,17 @@ def xyz_support(cache: dict):
 
     def apply_field(field):
         def _(p, x, xs):
-            cache.update({field : x})
+            cache.update({field: x})
+
         return _
 
     xyz_grid = grid_reference()
 
     extra_axis_options = [
         xyz_grid.AxisOption("[ReSharpen] Sharpness", float, apply_field("decay")),
-        xyz_grid.AxisOption("[ReSharpen] HrF. Sharpness", float, apply_field("hr_decay"))
+        xyz_grid.AxisOption(
+            "[ReSharpen] HrF. Sharpness", float, apply_field("hr_decay")
+        ),
     ]
 
     xyz_grid.axis_options.extend(extra_axis_options)
--- scripts/resharpen.py
+++ scripts/resharpen.py
@@ -9,6 +9,7 @@ import gradio as gr
 
 original_callback = KDiffusionSampler.callback_state
 
+
 def hijack_callback(self, d):
     if not self.trajectory_enable:
         return original_callback(self, d)
@@ -17,11 +18,12 @@ def hijack_callback(self, d):
         return original_callback(self, d)
 
     if self.traj_cache is not None:
-        delta = d['x'].detach().clone() - self.traj_cache
-        d['x'] += delta * self.traj_decay
+        delta = d["x"].detach().clone() - self.traj_cache
+        d["x"] += delta * self.traj_decay
 
-    self.traj_cache = d['x'].detach().clone()
+    self.traj_cache = d["x"].detach().clone()
     return original_callback(self, d)
+
 
 KDiffusionSampler.callback_state = hijack_callback
 
@@ -38,70 +40,86 @@ class ReSharpen(scripts.Script):
         return scripts.AlwaysVisible
 
     def ui(self, is_img2img):
-        with gr.Accordion('ReSharpen', open=False):
+        with gr.Accordion("ReSharpen", open=False):
             enable = gr.Checkbox(label="Enable")
 
-            gr.Markdown('<h3 style="float: left;">Softer</h3> <h3 style="float: right;">Sharper</h3>')
-            decay = gr.Slider(label="Sharpness", minimum=-1.0, maximum=1.0, step=0.05, value=0.0)
+            gr.Markdown(
+                '<h3 style="float: left;">Softer</h3> <h3 style="float: right;">Sharper</h3>'
+            )
+            decay = gr.Slider(
+                label="Sharpness", minimum=-1.0, maximum=1.0, step=0.1, value=0.0
+            )
 
             if not is_img2img:
-                hr_decay = gr.Slider(label="Hires. Fix Sharpness", minimum=-1.0, maximum=1.0, step=0.05, value=0.0)
+                hr_decay = gr.Slider(
+                    label="Hires. Fix Sharpness",
+                    minimum=-1.0,
+                    maximum=1.0,
+                    step=0.1,
+                    value=0.0,
+                )
 
-        self.paste_field_names = [
-            (enable, lambda d: enable.update(value=("Resharpen" in d))),
-            (decay, "Resharpen")
-        ]
+        self.paste_field_names = []
         self.infotext_fields = [
-            (enable, lambda d: enable.update(value=("Resharpen" in d))),
-            (decay, "Resharpen")
+            (enable, "Resharpen Enabled"),
+            (decay, "Resharpen Sharpness"),
         ]
+
+        for comp, name in self.infotext_fields:
+            comp.do_not_save_to_config = True
 
         if not is_img2img:
-              self.paste_field_names.append((hr_decay, "Resharpen HR"))
-              self.infotext_fields.append((hr_decay, "Resharpen HR"))
+            hr_decay.do_not_save_to_config = True
+            self.infotext_fields.append((hr_decay, "Resharpen Sharpness Hires"))
+            self.paste_field_names.append("Resharpen Sharpness Hires")
 
         if not is_img2img:
             return [enable, decay, hr_decay]
         else:
             return [enable, decay, decay]
 
-    def process(self, p, enable:bool, decay:float, hr_decay:float):
-        if 'decay' in self.XYZ_CACHE.keys():
+    def process(self, p, enable: bool, decay: float, hr_decay: float):
+        if "decay" in self.XYZ_CACHE:
             enable = True
-            decay = float(self.XYZ_CACHE['decay'])
-            del self.XYZ_CACHE['decay']
+            decay = float(self.XYZ_CACHE["decay"])
+            del self.XYZ_CACHE["decay"]
 
         KDiffusionSampler.trajectory_enable = enable
 
         if enable:
+            p.extra_generation_params["Resharpen Enabled"] = enable
 
-            if p.sampler_name.strip() == 'Euler a':
-                print('\n[Resharpen] has little effect when using an Ancestral sampler! Consider switching to Euler instead.\n')
+            if p.sampler_name.strip() == "Euler a":
+                print(
+                    "\n[Resharpen] has little effect when using an Ancestral sampler! Consider switching to Euler instead.\n"
+                )
 
             KDiffusionSampler.traj_decay = decay / -10.0
             KDiffusionSampler.traj_cache = None
-            p.extra_generation_params['Resharpen'] = decay
+            p.extra_generation_params["Resharpen Sharpness"] = decay
 
         return p
 
-    def before_hr(self, p, enable:bool, decay:float, hr_decay:float):
-        if 'hr_decay' in self.XYZ_CACHE.keys():
+    def before_hr(self, p, enable: bool, decay: float, hr_decay: float):
+        if "hr_decay" in self.XYZ_CACHE:
             enable = True
-            hr_decay = float(self.XYZ_CACHE['hr_decay'])
-            del self.XYZ_CACHE['hr_decay']
+            hr_decay = float(self.XYZ_CACHE["hr_decay"])
+            del self.XYZ_CACHE["hr_decay"]
 
         KDiffusionSampler.trajectory_enable = enable
 
-        if enable is True:
+        if enable:
+            p.extra_generation_params["Resharpen Enabled"] = enable
 
             KDiffusionSampler.traj_decay = hr_decay / -10.0
             KDiffusionSampler.traj_cache = None
-            p.extra_generation_params['Resharpen HR'] = hr_decay
+            p.extra_generation_params["Resharpen Sharpness Hires"] = hr_decay
 
         return p
 
 
 def restore_callback():
     KDiffusionSampler.callback_state = original_callback
+
 
 script_callbacks.on_script_unloaded(restore_callback)
